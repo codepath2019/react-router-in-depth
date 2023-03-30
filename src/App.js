ec0e06c696b7042ce4b0bbd90ce014142c07e151
Charles Magic Wooper <codepath2019@gmail.com>
2023-03-30 12:16:00 -0700

    set up project scaffold following lesson 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>JobaRouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* NOTE: specifying index is the same as path="/" */}
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
